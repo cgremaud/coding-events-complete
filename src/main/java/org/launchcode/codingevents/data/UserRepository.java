8173f52046059fb11dbb891704dfd42cecf19cc0
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-10 11:41:17 -0500

    added comments to UserRepository

--- src/main/java/org/launchcode/codingevents/data/UserRepository.java
+++ src/main/java/org/launchcode/codingevents/data/UserRepository.java
@@ -7,7 +7,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
-    //this is called a query method
+    //this is called a query method, Spring JPA just forms a SQL query out of it based on the wording of the method name.
     User findByUsername(String username); //returns a User object and takes a username as an arg
 
 }

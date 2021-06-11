e07434aa1f8b266f2f3e8cd0a17f81ea9d0157cd
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-11 09:55:21 -0500

    added register form and controller method

--- src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationController.java
@@ -2,8 +2,11 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.UserRepository;
 import org.launchcode.codingevents.models.User;
+import org.launchcode.codingevents.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
@@ -30,6 +33,13 @@ public class AuthenticationController {
 
     private static void setUserInSession(HttpSession session, User user) {
         session.setAttribute(userSessionKey, user.getId());
+    }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute(new RegisterFormDTO());
+        model.addAttribute("title", "Register");
+        return "register";
     }
 
 }

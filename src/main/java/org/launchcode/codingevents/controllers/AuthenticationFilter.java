209ab33db83403114c874507c60974a9274a8ae5
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-13 09:26:39 -0500

    implemented overridden preHandle method in AuthenticationFilter

--- src/main/java/org/launchcode/codingevents/controllers/AuthenticationFilter.java
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationFilter.java
@@ -1,12 +1,35 @@
 package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.UserRepository;
+import org.launchcode.codingevents.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class AuthenticationFilter {
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class AuthenticationFilter extends HandlerInterceptorAdapter {
     @Autowired
     UserRepository userRepository;
 
     @Autowired
     AuthenticationController authenticationController;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+
+        if (user != null) {
+            return true;
+        }
+        response.sendRedirect("/login");
+        return false;
+
+    }
+
 }

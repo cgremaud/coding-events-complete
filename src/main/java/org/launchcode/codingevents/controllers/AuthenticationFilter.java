c9ebc8b578b2b8ffd61c8082f58ca3d5985c684a
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-13 10:03:51 -0500

    completed AuthenticationFilter

--- src/main/java/org/launchcode/codingevents/controllers/AuthenticationFilter.java
+++ src/main/java/org/launchcode/codingevents/controllers/AuthenticationFilter.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuthenticationFilter extends HandlerInterceptorAdapter {
     @Autowired
@@ -18,8 +20,23 @@ public class AuthenticationFilter extends HandlerInterceptorAdapter {
     @Autowired
     AuthenticationController authenticationController;
 
+    private static final List<String> whitelist = Arrays.asList("/login", "/logout", "/register", "/css");
+
+    private static boolean isWhitelisted (String path) {
+        for (String pathRoot : whitelist) {
+            if (path.startsWith(pathRoot)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+
+        if (isWhitelisted(request.getRequestURI())) {
+            return true;
+        }
 
         HttpSession session = request.getSession();
         User user = authenticationController.getUserFromSession(session);
